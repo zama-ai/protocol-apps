09addb35a9f2d184007667238edaba4e3e563570
Seth-Schmidt <seth.schmidt@zama.ai>
2026-03-31 17:08:23 -0400

    fix: other typo

--- contracts/confidential-wrapper/contracts/interfaces/IERC7984ERC20Wrapper.sol
+++ contracts/confidential-wrapper/contracts/interfaces/IERC7984ERC20Wrapper.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-// Ported from Ported from  https://github.com/OpenZeppelin/openzeppelin-confidential-contracts/blob/7ac7cee5fec408dc81b31121f90417dfd87f3d13/contracts/interfaces/IERC7984ERC20Wrapper.sol (v0.4.0)
+// Ported from  https://github.com/OpenZeppelin/openzeppelin-confidential-contracts/blob/7ac7cee5fec408dc81b31121f90417dfd87f3d13/contracts/interfaces/IERC7984ERC20Wrapper.sol (v0.4.0)
 
 pragma solidity ^0.8.24;
 
