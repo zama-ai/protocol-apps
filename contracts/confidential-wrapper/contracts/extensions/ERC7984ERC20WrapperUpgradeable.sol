62de5d9aabdfc56eda9cd76fd9f936b225a3704f
Seth-Schmidt <seth.schmidt@zama.ai>
2026-03-31 13:11:18 -0400

    fix: remove unused ContextUpgradeable import in ERC7984ERC20WrapperUpgradeable

--- contracts/confidential-wrapper/contracts/extensions/ERC7984ERC20WrapperUpgradeable.sol
+++ contracts/confidential-wrapper/contracts/extensions/ERC7984ERC20WrapperUpgradeable.sol
@@ -5,7 +5,6 @@ pragma solidity ^0.8.27;
 
 import {FHE, externalEuint64, euint64} from "@fhevm/solidity/lib/FHE.sol";
 import {IERC1363Receiver} from "@openzeppelin/contracts/interfaces/IERC1363Receiver.sol";
-import {ContextUpgradeable} from "@openzeppelin/contracts-upgradeable/utils/ContextUpgradeable.sol";
 import {IERC20} from "@openzeppelin/contracts/interfaces/IERC20.sol";
 import {IERC20Metadata} from "@openzeppelin/contracts/interfaces/IERC20Metadata.sol";
 import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
